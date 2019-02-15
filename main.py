54bf0e93bcfa1e96f5ac2451ea284bac232bebd9
Chana030102 <chanaar@pdx.edu>
2019-02-15 12:24:00 -0800

    Add ROC curve and stats about SVM

--- main.py
+++ main.py
@@ -1,43 +1,48 @@
 
 from sklearn import svm
 import sklearn.metrics as metric
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import numpy
 
 #========= Data Import and Preprocessing ==========
 DATA_PATH = "../spambase.data"
 DELIMITER = ","
-data = numpy.loadtxt(DATA_PATH,delimiter=DELIMITER)
+data  = numpy.loadtxt(DATA_PATH,delimiter=DELIMITER)
+label = data[:,-1]
+data  = numpy.delete(data,-1,axis=1)
 
-# Split data in half: one for testing, one for training
-# keep labels in separate array and remove from samples
-index =round(len(data)) # get the index for half of the data 
-data_test = data[:index]
-label_test = data_test[:,-1]
-data_test = numpy.delete(data_test,-1,axis=1)
+# Randomize data and split in half for test and train sets
+traind, testd, trainl, testl = train_test_split(data, label, test_size=0.5, random_state=0)
 
-data_train = data[index:]
-label_train = data_train[:,-1]
-data_train = numpy.delete(data_train,-1,axis=1)
-
-# Scale training data
-mean = numpy.mean(data_train,axis=1) # Calculate mean of each feature (columns)
-std = numpy.std(data_train,axis=1)   # Calculate standard deviation of each feature
-scaled_train = (data_train-mean)/std
-
-# Scale test data using mean and std from training data
-scaled_test = (data_test-mean)/std
+# Scale train and test data with mean and std from train data
+mean = numpy.mean(traind,axis=0) # Calculate mean of each feature (columns)
+std = numpy.std(traind,axis=0)   # Calculate standard deviation of each feature
+mean = mean.reshape(1,-1)            # reshape into columns to scale data
+std = std.reshape(1,-1)
+scaled_train = (traind-mean)/std  
+scaled_test = (testd-mean)/std
 
 #========= Experiment 1 ==========
 model = svm.SVC(kernel="linear")
-model.fit(data_train,label_train)
-prediction = model.predict(data_test)
+score = model.fit(scaled_train,trainl).decision_function(scaled_test)
+prediction = model.predict(scaled_test)
 
-recall    = metric.recall_score(label_test,prediction)
-precision = metric.precision_score(label_test,prediction)
-accuracy  = metric.accuracy_score(label_test,prediction)
+fpr, tpr, _ = metric.roc_curve(testl,score)
+roc_auc = metric.auc(fpr,tpr)
+
+# Plot ROC curve
+plt.figure()
+plt.plot(fpr,tpr, lw=2, label='ROC curve (area=%0.2f)' % roc_auc)
+plt.xlabel('FPR')
+plt.ylabel('TPR')
+plt.title('ROC Curve - Spam Database')
+#plt.xlim([0.0,1.0])
+#plt.ylim([0.0,1.0])
+plt.show()
+
+recall    = metric.recall_score(testl,prediction)
+precision = metric.precision_score(testl,prediction)
+accuracy  = metric.accuracy_score(testl,prediction)
 
 print("Recall = {}%\tPrecision = {}%\tAccuracy = {}%".format(recall, precision,accuracy))
-
-
-
