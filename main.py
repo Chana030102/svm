e1bcd731ab4f6f0ae9be36c61201023072f4bbc0
Chana030102 <chanaar@pdx.edu>
2019-02-16 12:32:43 -0800

    Add complete assignment script

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ traind, testd, trainl, testl = train_test_split(data, label, test_size=0.5, rand
 # Scale train and test data with mean and std from train data
 mean = numpy.mean(traind,axis=0) # Calculate mean of each feature (columns)
 std = numpy.std(traind,axis=0)   # Calculate standard deviation of each feature
-mean = mean.reshape(1,-1)            # reshape into columns to scale data
+mean = mean.reshape(1,-1)        # reshape into columns to scale data
 std = std.reshape(1,-1)
 scaled_train = (traind-mean)/std  
 scaled_test = (testd-mean)/std
@@ -31,18 +31,62 @@ prediction = model.predict(scaled_test)
 fpr, tpr, _ = metric.roc_curve(testl,score)
 roc_auc = metric.auc(fpr,tpr)
 
+recall    = metric.recall_score(testl,prediction)
+precision = metric.precision_score(testl,prediction)
+accuracy  = metric.accuracy_score(testl,prediction)
+
+print("Experiment 1 Results:")
+print("Recall = {0:.2%}".format(recall))
+print("Precision = {0:.2%}".format(precision))
+print("Accuracy = {0:.2%}".format(accuracy))
+
 # Plot ROC curve
 plt.figure()
 plt.plot(fpr,tpr, lw=2, label='ROC curve (area=%0.2f)' % roc_auc)
 plt.xlabel('FPR')
 plt.ylabel('TPR')
 plt.title('ROC Curve - Spam Database')
-#plt.xlim([0.0,1.0])
-#plt.ylim([0.0,1.0])
 plt.show()
 
-recall    = metric.recall_score(testl,prediction)
-precision = metric.precision_score(testl,prediction)
-accuracy  = metric.accuracy_score(testl,prediction)
+#========= Experiment 2 ==========
+print("Experiment 2 Start")
+w = model.coef_   # Retrieve weight vector
+x = numpy.arange(2,58)
+a = []            # store accuracy
 
-print("Recall = {}%\tPrecision = {}%\tAccuracy = {}%".format(recall, precision,accuracy))
+top = numpy.argpartition(w,-5)[:,-5:].reshape(5)
+print("Top 5 features have the following indices: {}".format(top))
+
+for m in range(2,58):
+    index  = numpy.argpartition(w,-m)[:,-m:].reshape(m)
+    dtrain = scaled_train[:,index]
+    dtest = scaled_test[:,index]
+    model = svm.SVC(kernel="linear")
+    prediction = model.fit(dtrain, trainl).predict(dtest)
+    a.append(metric.accuracy_score(testl,prediction))
+
+plt.figure()
+plt.plot(x, a, lw=2)
+plt.xlabel('m')
+plt.ylabel('Accuracy')
+plt.title('Feature Selection Accuracy')
+plt.show()
+    
+
+#========= Experiment 3 ==========
+print("Experiment 3 Start")
+b = []    # store accuracy
+for m in range(2,58):
+    index = numpy.random.choice(57,m)
+    dtrain = scaled_train[:,index]
+    dtest = scaled_test[:,index]
+    model = svm.SVC(kernel="linear")
+    prediction = model.fit(dtrain, trainl).predict(dtest)
+    b.append(metric.accuracy_score(testl,prediction))
+
+plt.figure()
+plt.plot(x, b, lw=2)
+plt.xlabel('m')
+plt.ylabel('Accuracy')
+plt.title('Random Feature Selection Accuracy')
+plt.show()
